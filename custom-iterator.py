70fed53798118f8dcdb9d41372fccf6937d5a2a0
hanyseyedy <hanyseyedy@gmail.com>
2022-05-19 14:23:46 +0430

    Custom iterator- custom rang with step

--- custom-iterator.py
+++ custom-iterator.py
@@ -1,7 +1,8 @@
 class Counter:
-    def __init__(self, start, end):
+    def __init__(self, start, end, step=1):
         self.curent = start
         self.end = end
+        self.step = step
 
     def __iter__(self):
         return self
@@ -9,12 +10,12 @@ class Counter:
     def __next__(self):
         if self.curent < self.end:
             num = self.curent
-            self.curent += 1
+            self.curent += self.step
             return num
         raise StopIteration
 
 
-my_Counter = Counter(0, 20)
+my_Counter = Counter(0, 20, 2)
 
 for num in my_Counter:
     print(num)
