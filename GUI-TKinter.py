aa15ae7bc6da3a8d6d9359ed708e3b06bd836f60
hanyseyedy <hanyseyedy@gmail.com>
2022-05-24 16:29:04 +0430

    Entry in tkinter

--- GUI-TKinter.py
+++ GUI-TKinter.py
@@ -8,7 +8,7 @@ root.resizable(width=False, height=False)
 my_name = StringVar()
 
 def print_my_name():
-    my_name.set("my name is hany")
+    my_name.set(entry.get())
 
 label = Label(root, text="this is test label")
 label.place(x=50, y=50)
@@ -16,10 +16,16 @@ label.place(x=50, y=50)
 label2 = Label(root, text='this is second test label')
 label2.place(x=50, y=70)
 
-btn = Button(root, text="show my name", command=lambda: print_my_name())
-btn.place(x=50, y=100)
+label3 = Label(root, text="enter yor text: ")
+label3.place(x=50, y=100)
 
-label3 = Label(root, textvariable=my_name)
-label3.place(x=50, y=120)
+entry = Entry(root)
+entry.place(x=50, y=120)
+
+btn = Button(root, text="print input text", command=lambda: print_my_name())
+btn.place(x=50, y=150)
+
+label4 = Label(root, textvariable=my_name)
+label4.place(x=50, y=180)
 
 root.mainloop()
